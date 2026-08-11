2f0239df4386797e2c829173f1b701a7883cba8b
andreevajulia10-ux <andreevajulia10@gmail.com>
2026-08-11 08:42:20 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig =
+  process.env.STATIC_EXPORT === "1"
+    ? {
+        output: "export",
+        trailingSlash: true,
+        images: {
+          unoptimized: true,
+        },
+        typescript: {
+          tsconfigPath: "./tsconfig.static.json",
+        },
+      }
+    : {};
+
+export default nextConfig;
